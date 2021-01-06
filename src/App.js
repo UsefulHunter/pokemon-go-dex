c66650c56da84c24d17eac8d7eb744a76118fd1b
Brian Icaro D. Marques <bi.dm@hotmail.com>
2021-01-05 21:05:54 -0300

    new graphQL endpoint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { PokemonsContainer } from './containers/PokemonsContainer';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonsContainer } from "./containers/PokemonsContainer";
+import "./App.css";
 
 function App() {
   const client = new ApolloClient({
-    uri: 'https://graphql-pokemon.now.sh',
+    uri: "https://graphql-pokemon2.vercel.app",
   });
 
   return (
