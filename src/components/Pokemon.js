1668b2d1528bf2144eec58bc2aa9dc481c1f0904
Brian Icaro D. Marques <bi.dm@hotmail.com>
2021-01-05 21:41:43 -0300

    Changed layout to standardize the image position. Also, the attack colors now are dynamic, based on the pokemon attack types.

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
 export const Pokemon = ({ pokemon }) => {
   return (
     <div className="pokemon">
-      <div className="pokemon__name">
-        <p>
-          <span>#{pokemon.number} - </span>
-          {pokemon.name}
-        </p>
-      </div>
-      <div className="pokemon__types">
-        {pokemon?.types?.map((type) => (
-          <p key={`${type}`} className={`pokemon__common pokemon__${type}`}>
-            {type}
+      <div>
+        <div className="pokemon__name">
+          <p>
+            <span>#{pokemon.number} - </span>
+            {pokemon.name}
           </p>
-        ))}
-      </div>
-      <div className="pokemon__meta">
-        <span>{pokemon.maxHP} Max HP</span>
-        <span>{pokemon.maxCP} Max CP</span>
+        </div>
+        <div className="pokemon__types">
+          {pokemon?.types?.map((type) => (
+            <p key={`${type}`} className={`pokemon__common pokemon__${type}`}>
+              {type}
+            </p>
+          ))}
+        </div>
+        <div className="pokemon__meta">
+          <span>{pokemon.maxHP} Max HP</span>
+          <span>{pokemon.maxCP} Max CP</span>
+        </div>
       </div>
       <div className="pokemon__image">
         <img src={pokemon.image} alt={`this is ${pokemon.name}`} />
@@ -27,7 +29,7 @@ export const Pokemon = ({ pokemon }) => {
         {pokemon?.attacks?.special.slice(0, 3).map((attack) => (
           <span
             key={`${attack.name}-${attack.damage}-${attack.type}`}
-            className="pokemon__attack"
+            className={`pokemon__attack pokemon__${attack.type}`}
           >
             {attack.name}
           </span>
